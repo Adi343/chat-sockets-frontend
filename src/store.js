895cb6cd661d625d2b1d4d5c1ff1801f6966d0bf
Adithya <adithyapantangi123@gmail.com>
2022-09-02 23:11:50 +0530

    Removed zustand and store.js

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import create from "zustand/react";
-
-export const useStore = create((set) => ({
-  chatData: [],
-  addChat: (data) =>
-    set((state) => ({
-      chatData: [...state.chatData, data],
-    })),
-}));
